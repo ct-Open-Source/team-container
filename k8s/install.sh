6f7e9eda790c33869d887af3dfe5e3ed6ac8a448
jamct <jam@ct.de>
2020-03-20 17:16:28 +0100

    add k8s

---
+++ k8s/install.sh
@@ -0,0 +1,12 @@
+# Install and run K3S
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--no-deploy traefik" sh -
+
+# Install helm
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+export HELM_EXPERIMENTAL_OCI=1
+
+echo "KUBECONFIG=/etc/rancher/k3s/k3s.yaml">>/etc/environment
+echo "HELM_EXPERIMENTAL_OCI=1">>/etc/environment
+echo "Kubernetes is ready for teamcloud"
